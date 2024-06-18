5e6c719bae1fb1b2ccd8b2d393b00cfe44381b98
Morty245 <Roman.aleks1996@mail.ru>
2024-06-18 21:28:25 +0700

    Create new filE Java code (2 stap)

--- PhoneBook.java
+++ PhoneBook.java
@@ -18,3 +18,41 @@ public class PhoneBook {
             int choice = scanner.nextInt();
             scanner.nextLine();
             clearConsole();
+
+            switch (choice) {
+                case 1:
+                    displayItems();
+                    break;
+                case 2:
+                    addItem();
+                    break;
+                case 3:
+                    deleteItem();
+                    break;
+                case 4:
+                    editItem();
+                    break;
+                case 5:
+                    running = false;
+                    break;
+                default:
+                    System.out.println("\nНекорректный выбор! Попробуйте снова.\n");
+            }
+        }
+        System.out.println("\nРабота приложения завершена.\n");
+    }
+
+    private static void displayItems() {
+        if (phoneBook.isEmpty()) {
+            System.out.println("\nТелефонная книга пуста.\n");
+        } else {
+            List<Map.Entry<String, Set<String>>> sortedEntries = new ArrayList<>(phoneBook.entrySet());
+            sortedEntries.sort((entry1, entry2) -> Integer.compare(entry2.getValue().size(), entry1.getValue().size()));
+            hr();
+            System.out.println("Записи в телефонной книге (отсортированы по убыванию числа телефонов):\n");
+            for (Map.Entry<String, Set<String>> entry : sortedEntries) {
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+            }
+        }
+        waitEnter();
+    }
